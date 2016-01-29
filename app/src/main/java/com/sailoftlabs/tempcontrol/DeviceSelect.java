297335666a9e5a0db0f9d9be5cc9869210a11a83
davidpos <bigdavepos@gmail.com>
2016-01-29 11:37:31 -0800

    Add activity to show list of available devices for the user to select from

--- app/src/main/java/com/sailoftlabs/tempcontrol/DeviceSelect.java
+++ app/src/main/java/com/sailoftlabs/tempcontrol/DeviceSelect.java
@@ -1,13 +1,29 @@
 package com.sailoftlabs.tempcontrol;
 
+import android.app.AlertDialog;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+import io.particle.android.sdk.cloud.ParticleCloud;
+import io.particle.android.sdk.cloud.ParticleCloudException;
+import io.particle.android.sdk.cloud.ParticleCloudSDK;
+import io.particle.android.sdk.cloud.ParticleDevice;
+import io.particle.android.sdk.utils.Async;
+import io.particle.android.sdk.utils.Toaster;
+
 public class DeviceSelect extends AppCompatActivity {
+    ParticleCloud particleCloud;
+    private ArrayList<String> devices = new ArrayList<>();
+    private ParticleDevice myDevice;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,7 +31,7 @@ public class DeviceSelect extends AppCompatActivity {
         setContentView(R.layout.activity_device_select);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-
+        ParticleCloudSDK.init(DeviceSelect.this);
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -25,6 +41,51 @@ public class DeviceSelect extends AppCompatActivity {
             }
         });
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        particleCloud = ParticleCloudSDK.getCloud();
+
+        Async.executeAsync(particleCloud, new Async.ApiWork<ParticleCloud, Void>() {
+
+
+            @Override
+            public Void callApi(@NonNull final ParticleCloud particleCloud) throws ParticleCloudException, IOException {
+
+
+                final List<ParticleDevice> allDevices = particleCloud.getDevices();
+                for (ParticleDevice device : allDevices){
+                    devices.add(device.getID());
+
+                }
+                if (!devices.isEmpty()){
+
+                    myDevice = particleCloud.getDevice(devices.get(1));
+                    Toaster.l(DeviceSelect.this, "Device found" + " " + myDevice.getName());
+                }
+                else {
+                    Toaster.l(DeviceSelect.this,"No Devices");
+                }
+
+                return null;
+            }
+
+            @Override
+            public void onSuccess(Void aVoid) {
+                Toaster.l(DeviceSelect.this, "Success");
+
+            }
+
+            @Override
+            public void onFailure(ParticleCloudException e) {
+
+                AlertDialog.Builder builder = new AlertDialog.Builder(DeviceSelect.this);
+                builder.setMessage(R.string.device_select_error)
+                        .setTitle(R.string.login_error_title)
+                        .setPositiveButton(android.R.string.ok, null);
+                AlertDialog dialog = builder.create();
+                dialog.show();
+
+            }
+        });
+
     }
 
 }
