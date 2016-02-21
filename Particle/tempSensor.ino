59ed3caf60716dab1cf78709997d6ebb5977cd44
davidpos <bigdavepos@gmail.com>
2016-02-21 10:31:38 -0800

    Work on setting temp as a shared preference

--- Particle/tempSensor.ino
+++ Particle/tempSensor.ino
@@ -2,28 +2,41 @@
 
 
 double temperature = 0.0;
+double setTemp = 0.0;
 
 void setup()
 {
 
 
 
-    // register API variable
-    //Particle.variable("temperature", &temperature, DOUBLE);
+  // register API variable
+  Particle.variable("temperature", &temperature, DOUBLE);
+  Particle.variable("setTemp", &setTemp, DOUBLE);//will be set by app
+  Particle.function("set", requiredTemp);
 
-    pinMode(A5, INPUT);
+  pinMode(A5, INPUT);
 }
 
 void loop()
 {
-    delay(1000);
-    int analogValue = analogRead(A5);
-    double voltage = 3.3 * ((double)analogValue / 4095.0);
+  delay(1000);
+  int analogValue = analogRead(A5);
+  double voltage = 3.3 * ((double)analogValue / 4095.0);
 
 
-    temperature = (voltage - 0.5) * 100;
-    String conTemp = String(temperature, 2);
-    Spark.publish("temperature",conTemp);
+  temperature = (voltage - 0.5) * 100;
+  String conTemp = String(temperature, 2);
+  Particle.publish("temperature",conTemp);
+  if(temperature < setTemp){
+      Particle.publish("toohot", "Too Hot");
+  }
 
 
+
+}
+
+int requiredTemp(String command){
+    double temp = double(command);
+    setTemp = temp;
+    return 1;
 }
--- app/src/main/java/com/sailoftlabs/tempcontrol/TempSetActivity.java
+++ app/src/main/java/com/sailoftlabs/tempcontrol/TempSetActivity.java
@@ -1,7 +1,9 @@
 package com.sailoftlabs.tempcontrol;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
@@ -12,6 +14,7 @@ import android.widget.EditText;
 public class TempSetActivity extends AppCompatActivity {
     EditText tempText;
     String tempSet;
+    Double tempRequest;
 
 
     @Override
@@ -19,18 +22,24 @@ public class TempSetActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_temp_set);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-
+        SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(TempSetActivity.this);
         setSupportActionBar(toolbar);
         tempText = (EditText)findViewById(R.id.tempText);
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        tempSet = tempText.getText().toString();
-        if(tempSet.length() < 0){
 
-
-        }
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                tempSet = tempText.getText().toString();
+                if(tempSet.length() < 0){
+
+                    tempRequest = Double.parseDouble(tempSet);
+
+
+
+                    //ParticleCloudSDK.getCloud().publishEvent("SetTemp", tempSet, ParticleEventVisibility.PRIVATE, 60);
+
+                }
                 Snackbar.make(view, "Temp set", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
                 Intent intent = new Intent(TempSetActivity.this, MainActivity.class);
